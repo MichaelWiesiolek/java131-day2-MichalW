976c7d192fc9cf81ff86908377cc2d3c1d7ae2c7
MichaelWiesiolek <michal@moria24.pl>
2023-01-21 11:52:52 +0100

    Wapon

---
+++ Day2/src/main/java/WaponMagazine/Gun.java
@@ -0,0 +1,44 @@
+package WaponMagazine;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gun {
+    private int maxMagazineSize;
+    private List<String> magazine = new ArrayList<>();
+
+    public Gun(int magazineSize) {
+        this.maxMagazineSize = magazineSize;
+    }
+
+    public void loadBullet(String bulletName) {
+
+
+        if (maxMagazineSize <= magazine.size()) {
+            System.out.println("Przekroczony limit");
+            return;
+        }
+        magazine.add(bulletName);
+        System.out.println(magazine);
+    }
+
+    public boolean isLoaded() {
+        if (magazine.size() > 0) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public void shot() {
+        if (isLoaded()) {
+            System.out.println("Pif paf " + magazine.remove(magazine.size() - 1));
+         //   magazine.remove(magazine.size() - 1);
+        } else {
+            System.out.println("Pusty magazynek");
+        }
+    }
+}
+
--- Day2/src/main/java/WaponMagazine/Main.java
+++ Day2/src/main/java/WaponMagazine/Main.java
@@ -1,4 +1,31 @@
 package WaponMagazine;
 
 public class Main {
+    public static void main(String[] args) {
+        Gun gun = new Gun(6);
+        checkIfGunLoaded(gun);
+        gun.loadBullet("Bullet 1");
+        gun.loadBullet("Bullet 2");
+        gun.loadBullet("Bullet 3");
+        gun.loadBullet("Bullet 4");
+        gun.loadBullet("Bullet 5");
+        gun.loadBullet("Bullet 6");
+        gun.loadBullet("Bullet 7");
+
+        System.out.println("Broń naładowana: "+ gun.isLoaded());
+
+        gun.shot();
+        gun.shot();
+    }
+
+    private static void checkIfGunLoaded(Gun gun) {
+        System.out.println("Broń naładowana: "+ gun.isLoaded());
+    }
+
+  //  private static void loadWithBullet(Gun gun, int numberOfBullets){
+  //      for (int i = 0; i <= numberOfBullets ; i++) {
+  //          gun.loadBullet("Bullet" + 1);
+
+    //    }
+  //  }
 }
